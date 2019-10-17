4be2a8c5dc14a7a2913045d361913b6a8fd88a8d
Luc Leonard <git@luc-leonard.fr>
2019-10-17 19:59:04 +0200

    changes

--- rabbitmq-node/join_cluster.sh
+++ rabbitmq-node/join_cluster.sh
@@ -1,16 +1,30 @@
-rabbitmq-server &
-sleep 10s
-echo 'Cluster to join' $CLUSTER_NAME
-echo 'Wait for startup'
-rabbitmqctl await_startup
-echo 'Stopping app'
-rabbitmqctl stop_app
-echo 'Joingning cluster @ ' $CLUSTER_NAME
-rabbitmqctl join_cluster $CLUSTER_NAME
-echo 'Restart app'
-rabbitmqctl start_app
+leave_cluster()
+{
+	rabbitmqctl stop_app
+	echo 'Leaving cluster @ ' $CLUSTER_NAME
+	rabbitmqctl join_cluster $CLUSTER_NAME
+  	exit 1
+}
+
+
+join_cluster()
+{
+	rabbitmq-server &
+	sleep 10s
+	echo 'Cluster to join' $CLUSTER_NAME
+	echo 'Wait for startup'
+	rabbitmqctl await_startup
+	echo 'Stopping app'
+	rabbitmqctl stop_app
+	echo 'Joingning cluster @ ' $CLUSTER_NAME
+	rabbitmqctl join_cluster $CLUSTER_NAME
+	echo 'Restart app'
+	rabbitmqctl start_app
+}
+
+join_cluster
 echo 'Registering signal'
-trap '/bin/sh /leave_cluster.sh' TERM STOP
+trap leave_cluster 1 2 3 6
 while :
 do
 	sleep 60s
--- rabbitmq-node/leave_cluster.sh
+++
@@ -1,3 +0,0 @@
-rabbitmqctl stop_app
-echo 'Leaving cluster @ ' $CLUSTER_NAME
-rabbitmqctl join_cluster $CLUSTER_NAME
